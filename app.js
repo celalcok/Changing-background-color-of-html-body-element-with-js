5d7f958ba00864fb4c81e927a20a61d6fddc108f
celalcok <celalcok@gmail.com>
2022-05-16 01:51:58 +0300

    Added some extra features

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const body = document.querySelector('body');
 const button = document.querySelector('button');
-const colors = ['pink', 'crimson','red', 'green', 'yellow','purple'];
+const colors = ['pink', 'blue','red', 'green', 'yellow','purple'];
 const result = document.querySelector('#result');
 const radio1 = document.querySelector('#radio1');
 const radio2 = document.querySelector('#radio2');
-const radios = document.querySelectorAll('input');
+const inorder = document.querySelector('#in_order');
+const inorderCheck = document.querySelector('#in_order_check');
+const lblInOrder = document.querySelector('#lbl_in_order');
 let func;
-
+let number=0;
  
         // document.body.addEventListener('change', function (e) {
 
@@ -22,16 +24,39 @@ let func;
 
 if(radio1.checked === true) {
     func=changeBackgroundWithRGBColor;
+    inorder.classList.remove('show');
+    inorder.classList.add('hidden');
+
 }else if(radio2.checked === true) {
+    inorder.classList.add('show');
+    inorder.classList.remove('hidden');
     func=changeBackgroundWithTextColor;
+    
 }
-button.addEventListener('click', func, false);
+button.addEventListener('click', func);
+
+
 function changeBackgroundWithTextColor() {
-    const selectedNumber = Math.floor(Math.random()*colors.length);
+    let selectedNumber;
+    if(inorderCheck.checked === true){
+       selectedNumber = number;
+       lblInOrder.innerHTML="In Order (" + number+")";
+       console.log("order");
+       number++;
+       if(number >= colors.length){
+           number = 0;
+        }
+    }else{
+        lblInOrder.innerHTML="In Order ";
+        selectedNumber = Math.floor(Math.random()*colors.length);
+        console.log("random");
+    }
     const selectedColor = colors[selectedNumber];
-    body.style.background=selectedColor;
+    body.style.background = selectedColor;
     result.innerHTML = selectedColor;
+    
 }
+
 function changeBackgroundWithRGBColor() {
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
